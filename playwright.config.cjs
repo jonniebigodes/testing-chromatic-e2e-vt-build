91fe6dae88a38f2b1b8c01e6686fa37833d3085c
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2024-06-11 15:14:46 +0100

    Minor changes to the Playwright config

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -80,35 +80,7 @@ module.exports = defineConfig({
       name: "Mobile_ipad_Pro_11",
       use: { ...devices["iPad Pro 11"] },
     },
-    //
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
   webServer: {
     command: "yarn dev",
     url: "http://localhost:5173/",
